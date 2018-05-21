1ebb90ba004e0bb9381e5f86f10725dcd723fa45
Zhaowei Liu <1458531219@qq.com>
2018-05-21 13:36:49 +0800

    commit python structure

---
+++ python_dataStructure.py
@@ -0,0 +1,17 @@
+# _*_ coding utf-8 _*_
+a = 1
+b = 1.1
+c = "string"
+d = True
+e = None
+f = (1, 2)
+g = [1, 2]
+h = {'a':1, 'b':2}
+i = ([1,2])
+
+list = [a, b, c, d, e, f, g, h, i]
+for e in list:
+	try:
+		print(e)
+
+a = input("Stop")
